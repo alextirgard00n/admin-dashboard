b23cfb000a96718a3934839e5743afe1721d87fd
alextirgard00n <tirgardoon@gmail.com>
2023-03-30 20:32:41 -1000

    hamburger working

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const krabbyPatty = document.querySelector('.krabby-patty');
 const sidebar = document.querySelector('.sidebar');
 
 krabbyPatty.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-    alert('i dont know how to do this lol fml')
+    sidebar.classList.toggle('active');
 });
+
