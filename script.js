7a347b45ad083591448d5c1255a325242724a35c
alextirgard00n <tirgardoon@gmail.com>
2023-03-30 21:06:54 -1000

    hamburger element implemented

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const krabbyPatty = document.querySelector('.krabby-patty');
 const sidebar = document.querySelector('.sidebar');
+const closebtn = document.querySelector('.close');
+const listItems = document.querySelectorAll('li');
 
 krabbyPatty.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
+
+closebtn.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+});
+
+listItems.forEach(li => li.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+}));
+
+document.body.addEventListener("click", () => {
+    if (sidebar.classList.contains("active")) {
+        // sidebar.classList.remove("active");
+        console.log("help");
+    }
+});
+
+
 
