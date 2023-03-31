9c7b3323feb34c93dbdc20ece518037234f9ca25
alextirgard00n <tirgardoon@gmail.com>
2023-03-30 14:27:47 -1000

    implemented w/ hamburer, no function yet

---
+++ script.js
@@ -0,0 +1,7 @@
+const krabbyPatty = document.querySelector('.krabby-patty');
+const sidebar = document.querySelector('.sidebar');
+
+krabbyPatty.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+    alert('i dont know how to do this lol fml')
+});
