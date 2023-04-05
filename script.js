915f744701e0cee67d9b190793268edca4750e81
alextirgard00n <tirgardoon@gmail.com>
2023-04-04 19:23:11 -1000

    fixed sidebar activity

--- script.js
+++ script.js
@@ -2,16 +2,28 @@ const krabbyPatty = document.querySelector('.krabby-patty');
 const sidebar = document.querySelector('.sidebar');
 const closebtn = document.querySelector('.xbtn');
 const listItems = document.querySelectorAll('li');
+const main = document.querySelector('main');
 
-// toggles function on click
+//removes sidebar on mobile clicking anywhere on main
+main.addEventListener('click', mainToggle);
+
+// toggles sidebar on click
 krabbyPatty.addEventListener('click', togglebtn);
 closebtn.addEventListener('click', togglebtn);
 
+//closes sidebar if clicking any sidebar link
 listItems.forEach(li => li.addEventListener('click', togglebtn));
 
 function togglebtn() {
     sidebar.classList.toggle('active');
     closebtn.classList.toggle('close');
+}
+
+function mainToggle() {
+    if (sidebar.classList.contains('active')) {
+        sidebar.classList.remove('active')
+        closebtn.classList.remove('close')
+    }
 }
 
 
